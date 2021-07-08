56e03161c83591257c9185db62abee77f4b7d860
Denisse González Salas <denisseags@hotmail.com>
2021-07-07 22:50:37 -0600

    /v

--- vendor/dompdf/dompdf/src/Css/Stylesheet.php
+++ vendor/dompdf/dompdf/src/Css/Stylesheet.php
@@ -1658,7 +1658,7 @@ class Stylesheet
      */
     public static function getDefaultStylesheet()
     {
-        $dir = (__DIR__ . "/..");
+        $dir = (__DIR__ . "/../..");
        // $dir = realpath(__DIR__ . "/..");
         return $dir . self::DEFAULT_STYLESHEET;
     }
